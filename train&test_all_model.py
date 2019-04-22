5d7ab72dad8fffb885834b51dc2ea50debf39eca
Chaunhewie <ChaunhewieTian@163.com>
2019-04-22 19:13:40 +0800

    修正全部重跑的代码，去除CK+48数据训练，添加ACCNN模型

--- train&test_all_model.py
+++ train&test_all_model.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 import os
-enabled_nets = ["ACNN", "AlexNet"]
-enabled_datasets = ["JAFFE", "CK+48", "CK+", "FER2013"]
+enabled_nets = ["ACNN", "ACCNN", "AlexNet"]
+enabled_datasets = ["JAFFE", "CK+", "FER2013"]
 
 if __name__ == "__main__":
     for dataset in enabled_datasets:
